9cb7f2e31a937f6a7df03d4369e373ed894264a2
remitre-remi <remi.treier@gmail.com>
2026-03-19 10:15:48 +0200

    jess

--- single_car.php
+++ single_car.php
@@ -1,84 +1,82 @@
-<?php include("config.php"); ?>
+<?php include('config.php'); ?>
+
 <!doctype html>
 <html lang="et">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Auto detail</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
-
-<!-- NAVBAR -->
-<nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Autorent</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- menüü -->
+      <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="#">Autorent</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+    <a class="navbar-brand" href="index.php">Autorent</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
-    <div class="collapse navbar-collapse" id="menu">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a class="nav-link" href="#">Avaleht</a></li>
-        <li class="nav-item"><a class="nav-link active" href="#">Autod</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Hinnad</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control form-control-sm me-2" type="search" placeholder="Otsi autot">
-        <button class="btn btn-outline-secondary btn-sm">🔍</button>
+      <form class="d-flex" role="search" method="get" action="index.php">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi">
+        <button class="btn btn-outline-success" type="submit">Otsi</button>
       </form>
     </div>
   </div>
 </nav>
-
-<!-- SISU -->
-<div class="container my-5">
-
+    <!-- /menüü -->
+<!-- sisu -->
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+<!-- üks auto -->
 <?php
-$paring = "SELECT * FROM cars WHERE id=102";
-$valjund = mysqli_query($yhendus, $paring);
-$rida = mysqli_fetch_array($valjund);
-print_r($rida);
+    $paring = "SELECT * FROM cars";
+    if (!empty($_GET["otsi"])) {
+        $otsing = $_GET["otsi"];
+        $paring .= " WHERE mark LIKE '%".$otsing."%'";
+    } 
+    $paring .= " LIMIT 8";
+    // var_dump($_GET["otsi"]);
+
+    $valjund = mysqli_query($yhendus, $paring); //saadan päringu andmebaasi
+    while($rida = mysqli_fetch_assoc($valjund)){       //sikutan vastuse alla
+        // var_dump($rida);                            //kuvan testvastuse
 ?>
-
-
-  <div class="card shadow-sm">
-    <div class="row g-0">
-
-      <!-- Pilt -->
-      <div class="col-md-6">
-        <img src="https://loremflickr.com/800/500/audi"
-             class="img-fluid h-100 object-fit-cover rounded-start"
-             alt="Auto pilt">
-      </div>
-
-      <!-- Info -->
-      <div class="col-md-6">
-        <div class="card-body p-4">
-
-          <h3 class="card-title mb-3">Audi R8 Spyder</h3>
-          <p class="text-muted mb-4">Cabrio · 2021</p>
-
-          <ul class="list-unstyled mb-4">
-            <li><strong>Mootor:</strong> V10</li>
-            <li><strong>Kütus:</strong> Bensiin</li>
-            <li><strong>Käigukast:</strong> Automaat</li>
-            <li><strong>Kohad:</strong> 2</li>
-          </ul>
-
-          <h4 class="mb-3">250 € / päev</h4>
-
-          <button class="btn btn-dark w-100">Rendi</button>
-
+    <div class="col">
+        <div class="card">
+        <img src="https://loremflickr.com/400/250/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top" alt="<?php echo $rida["mark"]; ?>">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $rida["mark"]; ?> <?php echo $rida["model"]; ?></h5>
+            <p class="card-text">
+                Mootor: <?php echo $rida["engine"]; ?> <br>
+                Kütus: <?php echo $rida["fuel"]; ?><br>
+                Hind: <?php echo $rida["price"]; ?>€/päev<br>
+            </p>
+            <a href="single_car.php?id=<?php echo $rida["id"]; ?>" class="btn btn-dark w-100">Rendi</a>
         </div>
-      </div>
-
+        </div>
     </div>
-  </div>
-
+    <?php } ?>
+        <!-- /üks auto -->
+    </div>
 </div>
+<!-- /sisu -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
 </html>
