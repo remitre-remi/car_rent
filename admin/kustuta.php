7d928beadca370b689ee9ea683ab792718dc365a
remitre-remi <remi.treier@gmail.com>
2026-03-19 12:12:21 +0200

    jess

--- admin/kustuta.php
+++ admin/kustuta.php
@@ -2,7 +2,16 @@
 <?php include('../config.php'); ?>
 <?php
     if (!empty($_GET['delid'])) {
-        echo "hakkan kustutama";
+        $id = $_GET['delid'];
+        $paring = "DELETE FROM cars WHERE id=$id";
+        $valjund = mysqli_query($yhendus, $paring);
+        if ($valjund) {
+            echo "Kustutatud";
+            header("location:index.php");
+        } else {
+            echo "Error";
+        }   
+        
     }
 
 
