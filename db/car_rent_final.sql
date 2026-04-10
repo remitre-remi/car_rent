698dbf3961c0dc2b592cf134cec6d00467d344df
remitre-remi <remi.treier@gmail.com>
2026-04-10 11:17:22 +0300

    sedrg

--- db/car_rent_final.sql
+++ db/car_rent_final.sql
@@ -101,7 +101,7 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
