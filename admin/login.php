e28e0c32f03e46a0bb09825ae356d46b612cd7ee
remitre-remi <remi.treier@gmail.com>
2026-04-23 14:21:55 +0300

    dshg

--- admin/login.php
+++ admin/login.php
@@ -7,28 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-
 <?php
-    $msg ="";
+    $msg = "";
 
     if (!empty($_POST)) {
         $uname = $_POST['user'];
         $password = $_POST['password'];
-        $hash = '$2y$10$NvLe0B7CeYdhqIGInoRDgOEYgX9482tod77Zai4lCRgLtXE7TYORq';
+        $hash = '$2y$10$GShDgMs0q43XaQmeWGq0OekAo8RR2wsQWhAgXd4SlYeVwr0zoheaC';
 
         if ($uname=="admin" && password_verify($password, $hash)) {
-            echo "tere maailm";
-        } else {
+            echo "tere admin";
+        }else{
             $msg = "kasutaja vale";
         }
-
-
 
     }
 
 ?>
     <div class="container">
-        <div class="row">
+        <div class="row pt-4 mt-4">
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
                 <form method="post" action="login.php" autocomplete="off">
@@ -38,17 +35,16 @@
                     </div>
                     <div class="mb-3">
                         <label for="p" class="form-label">Password</label>
-                        <input name="password" type="password" class="form-control" id="p">
+                        <input name="password" type="password" class="form-control" id="p" >
                     </div>
-                    
                     <button type="submit" class="btn btn-primary">Logi sisse</button>
                 </form>
-                <?php= echo $msg; ?>
+                <?php echo $msg; ?>
             </div>
             <div class="col-sm-4"></div>
         </div>
     </div>
-
+  
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
