74d083674060dc8db018f2c6c6c83f9c5527f259
remitre-remi <remi.treier@gmail.com>
2026-04-23 12:34:20 +0300

    gfdjhdf

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,13 @@
     <div class="row row-cols-1 row-cols-md-4 g-4">
 <!-- üks auto -->
 <?php
+    // sõnumi kuvamine
+    if(isset($_GET['msg'])){
+      echo '<div class="alert alert-success" role="alert"> Kõik on hästi! </div>';
+    }
+
+
+  //autode kuvamine
     $paring = "SELECT * FROM cars";
     if (!empty($_GET["otsi"])) {
         $otsing = $_GET["otsi"];
@@ -17,14 +24,14 @@
     // var_dump($_GET["otsi"]);
 
     $valjund = mysqli_query($yhendus, $paring); //saadan päringu andmebaasi
-    
+
 ?>
 
 <table class="table">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">Mark></th>
+      <th scope="col">Mark</th>
       <th scope="col">Mudel</th>
       <th scope="col">Hind</th>
       <th scope="col">Hind</th>
@@ -37,14 +44,12 @@
       <th scope="col">Hind</th>
       <th scope="col">Kustuta</th>
       <th scope="col">Muuda</th>
-      
-      
     </tr>
   </thead>
   <tbody>
     <?php
         while($rida = mysqli_fetch_assoc($valjund)){       //sikutan vastuse alla
-        // var_dump($rida);                            //kuvan testvastuse
+            // var_dump($rida);                       //kuvan testvastuse
     ?>
     <tr>
       <th scope="row"><?php echo $rida["id"]; ?></th>
@@ -68,25 +73,8 @@
   </tbody>
 </table>
 
-
-
-    <!-- <div class="col"> -->
-        <!-- <div class="card">
-        <img src="https://loremflickr.com/400/250/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top" alt="<?php echo $rida["mark"]; ?>">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $rida["mark"]; ?> <?php echo $rida["model"]; ?></h5>
-            <p class="card-text">
-                Mootor: <?php echo $rida["engine"]; ?> <br>
-                Kütus: <?php echo $rida["fuel"]; ?><br>
-                Hind: <?php echo $rida["price"]; ?>€/päev<br>
-            </p>
-            <a href="single_car.php?id=<?php echo $rida["id"]; ?>" class="btn btn-dark w-100">Rendi</a>
-        </div>
-        </div>
-    </div>
-    
 </div>
-<!--/sisu-->
+<!-- /sisu -->
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
